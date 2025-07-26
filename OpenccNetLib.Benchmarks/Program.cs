9fcb5c7b7114883a87da490892216afd9ae9f4d4
laisuk <laisuk@yahoo.com>
2025-07-27 01:12:11 +0800

    Add project OpenccNetLib.Benchmarks

---
+++ OpenccNetLib.Benchmarks/Program.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Exporters.Csv;
+using BenchmarkDotNet.Running;
+
+namespace OpenccNetLib.Benchmarks
+{
+    [MemoryDiagnoser]
+    [SimpleJob(warmupCount: 1, iterationCount: 10)]
+    [MinColumn, MaxColumn, RankColumn]
+    [CsvExporter(CsvSeparator.Comma)]
+    [MarkdownExporter, RPlotExporter]
+    public class OpenccConvertBenchmarks
+    {
+        private Opencc? _opencc;
+        private string? _inputText;
+
+        [Params(100, 1_000, 10_000, 100_000, 1_000_000)]
+        public int Size;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _opencc = new Opencc("s2t");
+            var fullText = File.ReadAllText("Samples/QuanZhiDuZheShiJiao.txt");
+            _inputText = fullText[..Math.Min(Size, fullText.Length)];
+        }
+
+        [Benchmark]
+        public void BM_Convert_Sized()
+        {
+            var _ = _opencc!.Convert(_inputText!);
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<OpenccConvertBenchmarks>();
+        }
+    }
+}
